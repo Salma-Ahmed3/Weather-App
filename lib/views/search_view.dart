81cd595e23e2ad9457b553099bae1983630e04ad
Salma Ahmed <158223837+Salma-Ahmed3@users.noreply.github.com>
2024-04-18 00:53:16 +0200

    Update search_view.dart

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -1,9 +1,7 @@
-// import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/cubits/get_weather_cubit/get_weather_cubit.dart';
-// import 'package:weather_app/models/weather_model.dart';
-// import 'package:weather_app/services/weather_services.dart';
+
 
 class SearchView extends StatelessWidget {
   const SearchView({Key? key}) : super(key: key);
@@ -12,7 +10,6 @@ class SearchView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // backgroundColor: const Color(0xff79546E),
         title: const Text(
           'Search City',
         ),
@@ -22,14 +19,11 @@ class SearchView extends StatelessWidget {
         child: Center(
           child: TextField(
             onSubmitted: (value) async {
-              // -6	Trigger cubit
               var getWeatherCubit = BlocProvider.of<GetWeatherCubit>(context);
               getWeatherCubit.getWeather(CityName: value);
               Navigator.pop(context);
             },
-            // onChanged: (value) {
-            //   log(value);
-            // },
+           
             decoration: InputDecoration(
               contentPadding: const EdgeInsets.symmetric(
                 vertical: 25,
@@ -38,8 +32,6 @@ class SearchView extends StatelessWidget {
               labelText: 'Search',
               suffixIcon: const Icon(Icons.search),
               hintText: 'Enter City Name',
-              // hintStyle: TextStyle(backgroundColor: Colors.blue),
-              // prefixIcon: const Icon(Icons.search),
               suffixIconColor: Colors.red,
               border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(16),
