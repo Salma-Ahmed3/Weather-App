c61bac55d90eccc844f408af78fb8c1aad4ff291
Salma Ahmed <158223837+Salma-Ahmed3@users.noreply.github.com>
2024-04-18 00:55:28 +0200

    Update get_weather_cubit.dart

--- lib/cubits/get_weather_cubit/get_weather_cubit.dart
+++ lib/cubits/get_weather_cubit/get_weather_cubit.dart
@@ -3,15 +3,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/cubits/get_weather_cubit/get_weather_states.dart';
 import 'package:weather_app/models/weather_model.dart';
 import 'package:weather_app/services/weather_services.dart';
-// import 'package:weather_app/views/search_view.dart';
 
-// 2-create cubit
 
 class GetWeatherCubit extends Cubit<WeatherState> {
   GetWeatherCubit() : super(WeatherInitialState());
 
   WeatherModel? weatherModel;
-  // 3-function
 
   getWeather({required String CityName}) async {
     try {
